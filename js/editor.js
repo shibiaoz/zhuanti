f4673bcb751906b4934f24138e7e16bf6eccc805
shibiaoz <shibiaoz@sina.cn>
2015-12-29 20:25:01 +0800

    backup

---
+++ js/editor.js
@@ -0,0 +1,56 @@
+/***********
+* 编辑功能
+* name: Editor
+* date: 2015-12-29
+* author: Knight
+* github: https://github.com/shibiaoz
+*
+*
+* 组件的配制到时候需要存到数据库中
+*
+*
+* 后台：
+*     1. 创建组件、配置组件需要的数据格式，
+*     2. 数据格式一定要和组件保持一致
+*
+* <p data-html></p>
+* {
+*     name:"html",
+*     type:
+*
+* }
+*
+**************/
+;(function  ($) {
+
+    function Editor () {
+        this.config = config;
+    }
+
+
+    /**
+     * 根据配置生成填写表单
+     */
+    Editor.prototype.buildForm = function  () {
+
+    }
+
+    /**
+     * 得到form中表单填写的数据
+     *
+     */
+
+    Editor.prototype.getForm = function  () {
+
+    }
+
+    /**
+     * 把数据写会组件
+     *
+     */
+    Editor.prototype.setData = function  () {
+
+    }
+
+
+})(jQuery);
